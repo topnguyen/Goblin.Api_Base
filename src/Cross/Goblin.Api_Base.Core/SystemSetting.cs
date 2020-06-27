5758f50536acf132a794b159d6183ad23dfea75a
Top Nguyen <admin@topnguyen.com>
2020-06-27 08:30:08 +0700

    Re-Initial

--- src/Cross/Goblin.Api_Base.Core/SystemSetting.cs
+++ src/Cross/Goblin.Api_Base.Core/SystemSetting.cs
@@ -9,21 +9,5 @@ namespace Goblin.Api_Base.Core
         /// </summary>
         /// <remarks>Use for protect Service in Public Network, empty or null for allow anonymous.</remarks>
         public string AuthorizationKey { get; set; }
-
-        public int ImageMaxWidthPx { get; set; }
-
-        public int ImageMaxHeightPx { get; set; }
-
-        public int ImageSkeletonMaxWidthPx { get; set; }
-
-        public int ImageSkeletonMaxHeightPx { get; set; }
-
-        public int ImageThumbnailMaxWidthPx { get; set; }
-
-        public int ImageThumbnailMaxHeightPx { get; set; }
-
-        public string Api_BaseFolderPath { get; set; }
-
-        public string DefaultFolderName { get; set; }
     }
 }
